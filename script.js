046b6e579656fff348cfe1c091d3aa9384152b3d
raquelledianne <raquelledschoolcraft@gmail.com>
2026-02-22 05:59:03 -0800

    save5

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Wait until the HTML page fully loads before running this code
+
 document.addEventListener("DOMContentLoaded", function () {
 
-  // Connects JavaScript to your HTML  
+    
   const searchInput = document.getElementById("search__input");
   const moviesDiv = document.getElementById("movies");
   const loadingDiv = document.getElementById("loading");
 
-  // Run this function every time the user types something
-  // .value → gets what the user typed
-  // .trim() → removes extra spaces
+
   searchInput.addEventListener("input", function () {
     const searchTerm = searchInput.value.trim();
 
-    // Stop if less than 2 characters
-    if (searchTerm.length < 2) {
-      moviesDiv.innerHTML = "";
-      return;
-    }
+  if (searchTerm.length < 2) {
+  moviesDiv.innerHTML = "";
+  return;
+}
 
     fetchMovies(searchTerm);
   });
@@ -30,12 +27,12 @@ document.addEventListener("DOMContentLoaded", function () {
         `https://www.omdbapi.com/?apikey=d8cfd282&s=${searchTerm}`
       );
 
-      // This turns it into a JavaScript object you can use.
+      
       const data = await response.json();
 
       loadingDiv.textContent = "";
 
-        // Checking to see if movies exist (true vs false)
+       
       if (data.Response === "True") {
         displayMovies(data.Search);
       } else {
@@ -51,25 +48,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function displayMovies(movies) {
 
-    const firstSixMovies = movies.slice(0, 6);
+  const firstSixMovies = movies.slice(0, 6);
 
-    // Go through each movie one by one
-    firstSixMovies.forEach(movie => {
-
-    // This creates a new <div> in memory.
-      const movieCard = document.createElement("div");
-      movieCard.classList.add("movie");
-
-      movieCard.innerHTML = `
+  moviesDiv.innerHTML = firstSixMovies
+    .map(movie => `
+      <div class="movie">
         <h3>${movie.Title}</h3>
         <p>${movie.Year}</p>
         <img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/200"}" alt="${movie.Title}">
-      `;
-
-      // This puts the movie card inside your HTML so it appears on the website
-      moviesDiv.appendChild(movieCard);
-    });
-  }
+      </div>
+    `)
+    .join("");
+}
 
 });
 
